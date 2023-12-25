319059b667e3a84395d9425cda4ec6fb9f8edf2c
Geetanjali Kandpal <79351146+geetanjalikandpal21@users.noreply.github.com>
2023-12-25 21:50:01 +0530

    Create Code.java

---
+++ Given a linked list of 0s, 1s and 2s, sort it./Code.java
@@ -0,0 +1,117 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Driverclass
+{
+    public static void main (String[] args) throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t = Integer.parseInt(in.readLine().trim());
+        
+        while(t-- > 0)
+        {
+            int n =Integer.parseInt(in.readLine().trim());
+            String s[] = in.readLine().trim().split(" ");
+            Node head = new Node(Integer.parseInt(s[0]));
+            Node tail = head;
+            for (int i = 1; i < n; i++) {
+                tail.next = new Node(Integer.parseInt(s[i]));
+                tail = tail.next;
+            }
+
+            head = new Solution().segregate(head);
+            printList(head, out);
+            out.println();
+        }
+        out.close();
+    }
+    
+    public static void printList(Node head,PrintWriter out)
+    {
+        if(head == null)
+           return;
+           
+        Node temp = head;
+        while(temp != null)
+        {
+            out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+    
+    
+}
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int data)
+    {
+        this.data = data;
+        next = null;
+    }
+}
+*/
+class Solution {
+    //Function to sort a linked list of 0s, 1s, and 2s.
+    static Node segregate(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        Node zeroHead = new Node(0);
+        Node oneHead = new Node(0);
+        Node twoHead = new Node(0);
+        Node zeroTail = zeroHead;
+        Node oneTail = oneHead;
+        Node twoTail = twoHead;
+
+        Node current = head;
+        while (current != null) {
+            if (current.data == 0) {
+                zeroTail.next = current;
+                zeroTail = zeroTail.next;
+            } else if (current.data == 1) {
+                oneTail.next = current;
+                oneTail = oneTail.next;
+            } else {
+                twoTail.next = current;
+                twoTail = twoTail.next;
+            }
+            current = current.next;
+        }
+
+        // Connect the three segments
+        zeroTail.next = (oneHead.next != null) ? oneHead.next : twoHead.next;
+        oneTail.next = twoHead.next;
+        twoTail.next = null;
+
+        return zeroHead.next;
+    }
+}
