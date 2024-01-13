7c92a7250f42368e1c884c0e7750ac5683f7e280
Geetanjali Kandpal <79351146+geetanjalikandpal21@users.noreply.github.com>
2024-01-13 23:52:07 +0530

    Create Code.java

---
+++ Insertion Sort for Singly Linked List/Code.java
@@ -0,0 +1,111 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+class insertion
+{
+    Node head;  
+    Node tail;
+	public void addToTheLast(Node node) 
+	{
+	  if (head == null) 
+	  {
+	   head = node;
+	   tail = node;
+	  } 
+	  else 
+	  {
+	   tail.next = node;
+	   tail = node;
+	  }
+	}
+      void printList(Node head)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	/* Drier program to test above functions */
+	public static void main(String args[])
+    {
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 while(t>0)
+         {
+			int n = sc.nextInt();
+			insertion llist = new insertion(); 
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+				llist.addToTheLast(new Node(a));
+			}
+			
+        Solution ob = new Solution();
+		head = ob.insertionSort(llist.head);
+		llist.printList(head);
+		
+        t--;		
+        }
+    }}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+    */
+class Solution
+{
+    public static Node insertionSort(Node head_ref)
+    {
+        if (head_ref == null || head_ref.next == null) {
+            return head_ref;
+        }
+
+        Node sortedList = null; // This will be our new sorted list
+
+        Node current = head_ref;
+        while (current != null) {
+            Node next = current.next; // Store next for the next iteration
+            sortedList = sortedInsert(sortedList, current); // Insert current in sorted list
+            current = next; // Move to next node
+        }
+
+        return sortedList;
+    }
+
+    // Function to insert a node into the sorted linked list
+    private static Node sortedInsert(Node sortedList, Node newNode) {
+        if (sortedList == null || sortedList.data >= newNode.data) {
+            newNode.next = sortedList;
+            return newNode;
+        }
+
+        Node current = sortedList;
+        while (current.next != null && current.next.data < newNode.data) {
+            current = current.next;
+        }
+
+        newNode.next = current.next;
+        current.next = newNode;
+        return sortedList;
+    }
+}
